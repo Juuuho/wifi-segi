a36b0b1038d845934849ae86b5780f4e9c81ce9d
Juuuho <gjwngh02@gmail.com>
2023-02-06 02:08:04 +0900

    change handler

--- wifi-segi.py
+++ wifi-segi.py
@@ -1,16 +1,30 @@
-import sys
+import codecs
 from scapy.all import *
 
 
-ap_list = []
+def handler(p):
+    if not (p.haslayer(Dot11Beacon)):
+        return
 
-def PacketHandler(packet):
-    if packet.haslayer(Dot11):
-        if packet.type == 0 and packet.subtype == 8:
-            if packet.addr2 not in ap_list:
-                ap_list.append(packet.addr2)
-                print("Access Point MAC: %s with SSID: %s " %(packet.addr2, packet.info))
+    rssi = p[RadioTap].dBm_AntSignal
+    dst_mac = p[Dot11].addr1
+    src_mac = p[Dot11].addr2
+    ap_mac = p[Dot11].addr2
+    info = f"rssi={rssi:2}dBm, dst={dst_mac}, src={src_mac}, ap={ap_mac}"
+
+    if p.haslayer(Dot11ProbeResp):
+        ssid = codecs.decode(p[Dot11Elt].info, 'utf-8')
+        channel = ord(p[Dot11Elt:3].info)
+        print(f"[ProbResp] {info}, chan={channel}, ssid=\"{ssid}\"")
+    elif p.haslayer(Dot11ProbeReq):
+        print(f"[ProbReq ] {info}")
+    elif p.haslayer(Dot11Beacon):
+        stats = p[Dot11Beacon].network_stats()
+        ssid = str(stats['ssid'])
+        channel = ord(p[Dot11Elt:3].info)
+        interval = p[Dot11Beacon].beacon_interval
+        print(f"[Beacon  ] {info}, chan={channel}, interval={interval}, ssid=\"{ssid}\"")
 
 
 if __name__ == "__main__":
-    sniff(iface="mon0", prn=PacketHandler)
+    sniff(iface="mon0", prn=handler, store=0)
