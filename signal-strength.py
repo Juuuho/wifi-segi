3b1e1611a885da133acf4e9d5b0db2a70d0022e2
Juuuho <gjwngh02@gmail.com>
2023-02-06 10:20:57 +0900

    add argument

--- signal-strength.py
+++ signal-strength.py
@@ -1,15 +1,30 @@
 import codecs
+import argparse
 from scapy.all import *
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
+mac = ""
+cnt = 0
+plt.style.use('fivethirtyeight')
+
+x_val = []
+y_val = []
 
 def handler(p):
-    if not (p.haslayer(Dot11Beacon)):
+    global cnt
+    if not (p.haslayer(Dot11ProbeResp) or p.haslayer(Dot11ProbeReq) or p.haslayer(Dot11Beacon)):
         return
 
     rssi = p[RadioTap].dBm_AntSignal
     dst_mac = p[Dot11].addr1
     src_mac = p[Dot11].addr2
     ap_mac = p[Dot11].addr2
+    
+    if src_mac != mac:
+        return
+    
+    cnt += 1
     info = f"rssi={rssi:2}dBm, dst={dst_mac}, src={src_mac}, ap={ap_mac}"
 
     if p.haslayer(Dot11ProbeResp):
@@ -27,4 +42,13 @@ def handler(p):
 
 
 if __name__ == "__main__":
-    sniff(iface="mon0", prn=handler, store=0)
+    
+    argument = sys.argv
+    del argument[0]
+    print(f'Argument : {argument}')
+
+    if len(argument) != 2:
+        print("syntax : python signal-strength.py <interface> <mac>\nsample : signal-strength.py mon0 00:11:22:33:44:55")
+        quit()
+    mac = argument[1].lower()
+    sniff(iface=argument[0], prn=handler, store=False)
